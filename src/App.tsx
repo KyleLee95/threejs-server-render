fde9c7cc2ea244f86fb13d64773aa68f1de635a1
kyle <kyle@kylelee.dev>
2023-05-23 16:28:24 -0500

    fixed commented out variable

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ channel.onConnect((error) => {
 });
 
 //***************************WEBRTC***********/
-// let remoteStream: any;
+let remoteStream: any;
 let remoteVideo: any;
 let peerConnection: any;
 let uuid: any;
@@ -143,23 +143,14 @@ function createUUID() {
   );
 }
 function App() {
-  const [count, setCount] = useState(0);
   useEffect(() => {
     pageReady();
   }, []);
   return (
-    <>
-      <button onClick={() => start(true)}> Start </button>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+    <div>
+      <h1>Press start to start stream</h1>
 
+      <button onClick={() => start(true)}> Start </button>
       <video id="localVideo" />
 
       <video
@@ -168,18 +159,7 @@ function App() {
         muted
         style={{ height: "100%", width: "100%" }}
       />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
 
